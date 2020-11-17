dc46d462f4921f89250c73d90970f23acb254eeb
RomanPalna <63646611+RomanPalna@users.noreply.github.com>
2020-11-17 17:32:39 +0200

    update API

--- src/js/film-api.js
+++ src/js/film-api.js
@@ -1,20 +1,26 @@
 const BASE_URL = 'https://api.themoviedb.org/3/trending/all/week?';
 
 export default class filmApi {
-  constructor() {}
-
+  constructor() {
+    this.page = 1;
+  }
   async fetchFilms() {
     const searchParams = new URLSearchParams({
       media_type: 'all',
       time_window: 'day',
+      page: this.page,
       api_key: '212da8d7e84ef8a0df2f733afbf10d5d',
     });
 
-    const url = `${BASE_URL}?${searchParams}`;
+    const url = `${BASE_URL}${searchParams}`;
 
     const response = await fetch(url);
     const movies = await response.json();
 
     return movies;
+  }
+
+  incrementPage() {
+    this.page += 1;
   }
 }
