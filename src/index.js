ec3a60cd81c7a54b41202619113a0b2fdf9dee94
Grigoriy Chernish <cga.jsdev@gmail.com>
2020-11-19 22:12:55 +0200

    mainMarkup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import './sass/main.css';
 import FilmApi from './js/film-api';
 import debounce from 'lodash.debounce';
+import filmCardTpl from './templates/filmCard.hbs'
+
+const refs = {
+  galery: document.querySelector('.js-galery')
+}
 
 const filmApi = new FilmApi();
+console.log(filmApi)
 
-filmApi.fetchFilms();
+filmApi.fetchFilms().then(movies => {
+  renderStartMarkup(movies);
+});
+
+function createMenuMoviesMarkup (movies)  {
+  return movies.map(filmCardTpl).join('');
+}
+
+function renderStartMarkup(movies) {
+  refs.galery.innerHTML = createMenuMoviesMarkup(movies);
+}
