c34acb9bdf0dd99c1888cc1f2db4281b46e38e98
Roman Palna <roma.palna@gmail.com>
2020-11-22 20:02:42 +0200

    local storage add watched films

--- src/js/storage-api.js
+++ src/js/storage-api.js
@@ -1,15 +1,35 @@
-export default class StorageApi {
-    constructor() {
-        console.log('class storageapi');
-    }
+import FilmApi from './film-api';
+const filmApi = new FilmApi();
 
-    
-    addToQueue(id){
-        console.log(id);
-        console.log('storage. addTo que');
+export default class StorageApi {
+  constructor() {
+    const WATCHED_FILMS = 'watched';
+    console.log('class storageapi');
+
+    console.log('class storageapi');
+    this.watchArray = [];
+  }
+
+  addToQueue(id) {
+    console.log(id);
+
+    console.log('storage. addTo que');
+  }
+
+  addToWatched(movie) {
+    console.log(movie);
+    console.log('Register click');
+
+    const watchedFilms = JSON.parse(localStorage.getItem('WatchedFilm'));
+
+    if (watchedFilms !== null) {
+      watchedFilms[movie.id] = movie;
+      localStorage.setItem('WatchedFilm', JSON.stringify(watchedFilms));
+    } else {
+      localStorage.setItem(
+        'WatchedFilm',
+        JSON.stringify({ [movie.id]: movie }),
+      );
     }
-    addToWatched(id) {
-        console.log(id);
-        console.log('storage. add to watched');
-    }
+  }
 }
