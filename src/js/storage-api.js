f92aa03a9ee7c48a542751b14cb29929c257b2e9
darya-ungvari <63539808+darya-ungvari@users.noreply.github.com>
2020-11-22 16:26:35 +0300

    add que

--- src/js/storage-api.js
+++ src/js/storage-api.js
@@ -1,3 +1,6 @@
+const WATCHED_KEY = 'w1';
+const QUEUE_KEY = 'ku1'
+
 export default class StorageApi {
     constructor() {
         console.log('class storageapi');
@@ -5,6 +8,21 @@ export default class StorageApi {
 
     
     addToQueue(id){
+    
+       let existedItems = localStorage.getItem(QUEUE_KEY);
+       let parsedItems;
+       if (existedItems) {
+            parsedItems = JSON.parse(existedItems)
+       } else {
+           parsedItems = [];
+       }
+       parsedItems.push(id);
+       let uniqItems = parsedItems.filter(function (x, i, a) { 
+           return a.indexOf(x) == i; 
+       });
+       localStorage.setItem(QUEUE_KEY, JSON.stringify(uniqItems));
+       console.log(localStorage.getItem(QUEUE_KEY));
+
         console.log(id);
         console.log('storage. addTo que');
     }
